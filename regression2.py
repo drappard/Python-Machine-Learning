4f27218b114b5ec3a55c79e4005582bccca4fbb8
Daniel Rappard <drappard@g.emporia.edu>
2019-01-31 12:05:59 -0600

    update

--- regression2.py
+++ regression2.py
@@ -5,7 +5,7 @@
 import quandl
 import pandas as pd
 
-df = Quandl.get("WIKI/GOOGL")
+df = quandl.get("WIKI/GOOGL")
 df = df[['Adj. Open',  'Adj. High',  'Adj. Low',  'Adj. Close', 'Adj. Volume']]
 df['HL_PCT'] = (df['Adj. High'] - df['Adj. Low']) / df['Adj. Close'] * 100.0
 df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
