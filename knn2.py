2506dc4f2e5c0bc3e64a1eb0caa65fe63f5ea4a1
Daniel Rappard <drappard@g.emporia.edu>
2019-02-02 21:20:42 -0600

    updated and reorganized knn2, finished knn algo, saved plots

--- knn2.py
+++ knn2.py
@@ -1,5 +1,8 @@
 #Python Machine Learning Tutorial
-#Part 15 - Creating a K Nearest Neighbors Classifer from scratch
+#Part 16-17 - Creating a K Nearest Neighbors Classifer from Scratch Part 1-2
+
+#Euclidean Distance - length of a line segment between two points
+#Euclidean Norm - measure of the magnitude of a vector
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,6 +12,22 @@ from math import sqrt
 from collections import Counter
 style.use('fivethirtyeight')
 
+def k_nearest_neighbors(data, predict, k=3):
+    if len(data) >= k:
+        warnings.warn('K is set to a value is less than total voting groups!')
+
+    distances = []
+    for group in data:
+        for features in data[group]:
+            #euclidean_distance = sqrt( (features[0]-predict[0])**2 + (features[1]-predict[1])**2 )
+            #euclidean_distance = np.sqrt(np.sum((np.array(features)-np.array(predict))**2))
+            euclidean_distance = np.linalg.norm(np.array(features)-np.array(predict))
+            distances.append([euclidean_distance,group])
+            #print(euclidean_distance)
+    votes = [i[1] for i in sorted(distances)[:k]]
+    vote_result = Counter(votes).most_common(1)[0][0]
+    return vote_result
+
 dataset = {'k':[[1,2],[2,3],[3,1]], 'r':[[6,5],[7,7],[8,6]]}
 new_features = [5,7]
 
@@ -16,6 +35,10 @@ new_features = [5,7]
 for i in dataset:
     for ii in dataset[i]:
         plt.scatter(ii[0], ii[1], s=100, color=i)
+
 plt.scatter(new_features[0], new_features[1], s=100)
 
+result = k_nearest_neighbors(dataset, new_features)
+plt.scatter(new_features[0], new_features[1], s=100, color = result) 
+print(result)
 plt.show()
