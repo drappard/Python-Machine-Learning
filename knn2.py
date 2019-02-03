8f0131cd01c5b413cd5f705eaf2f496629786129
Daniel Rappard <drappard@g.emporia.edu>
2019-02-02 19:50:35 -0600

    Update knn2.py

--- knn2.py
+++ knn2.py
@@ -2,17 +2,20 @@
 #Part 15 - Creating a K Nearest Neighbors Classifer from scratch
 
 import numpy as np
-import matplotlib.pyplot as pyplot
+import matplotlib.pyplot as plt
 from matplotlib import style
 import warnings
 from math import sqrt
-from collections import counter
+from collections import Counter
 style.use('fivethirtyeight')
 
 dataset = {'k':[[1,2],[2,3],[3,1]], 'r':[[6,5],[7,7],[8,6]]}
-new_features = [5.7]
+new_features = [5,7]
 
-[[plt.scatter(ii[0],ii[1],s=100,color=i) for ii in dataset[i]] for i in dataset]
-plt.scatter(new_features[0], new_features[1],s=100)
+#[[plt.scatter(ii[0],ii[1],s=100,color=i) for ii in dataset[i]] for i in dataset]
+for i in dataset:
+    for ii in dataset[i]:
+        plt.scatter(ii[0], ii[1], s=100, color=i)
+plt.scatter(new_features[0], new_features[1], s=100)
 
 plt.show()
