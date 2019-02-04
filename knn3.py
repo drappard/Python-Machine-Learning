64a73a797a037eea013f97f05788766058d629a0
Daniel Rappard <drappard@g.emporia.edu>
2019-02-04 13:51:35 -0600

    added matplotlib import, populate dictionaries, iterate thru data

--- knn3.py
+++ knn3.py
@@ -2,10 +2,13 @@
 #Part 18 - Testing our K Nearest Neighbors Classifier
 
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import style
 import warnings
 from collections import Counter
 import pandas as pd
 import random
+style.use('fivthirtyeight')
 
 def k_nearest_neighbors(data, predict, k=3):
     if len(data) >= k:
@@ -26,6 +29,7 @@ df.drop(['id'],1,inplace=True)
 full_data = df.astype(float).values.tolist()
 
 random.shuffle(full_data)
+
 test_size = 0.2
 #prepare dictionaries for training and testing to be populated
 #dictioanries have two keys: 2 = benign tumors, 4 = malinant tumors
@@ -35,3 +39,23 @@ test_set = {2:[], 4:[]}
 train_data = full_data[:-int(test_size*len(full_data))]
 #select last 20% as testing data
 test_data = full_data[-int(test_size*len(full_data)):]
+
+#populate dictionaries, key is the class, values are the attributes
+for i in train_data:
+    train_set[i[-1]].append(i[:-1])
+
+for i in test_data:
+    test_set[i[-1]].append(i[:-1])
+
+correct = 0
+total = 0
+
+#iterate through both classes/keys in dictionary
+for group in test_set:
+    #iterate through each datapoint in each class
+    for data in test_set[group]:
+        vote = k_nearest_neighbors(train_set, data, k=5)
+        if group == vote
+            correct += 1
+        total += 1
+print('Accuracy:', correct/total)
