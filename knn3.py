02ff67639c4f0ce71baa72ab8110b9b15b2df73c
Daniel Rappard <drappard@g.emporia.edu>
2019-02-04 12:56:44 -0600

    import data and define train and test set

--- knn3.py
+++ knn3.py
@@ -18,3 +18,20 @@ def k_nearest_neighbors(data, predict, k=3):
     votes = [i[1] for i in sorted(distances)[:k]]
     vote_result = Counter(votes).most_common(1)[0][0]
     return vote_result
+
+df = pd.read_csv('breast-cancer-wisconsin.data')
+df.replace('?',-99999,inplace=True)
+df.drop(['id'],1,inplace=True)
+#convert data to list of list and all float values
+full_data = df.astype(float).values.tolist()
+
+random.shuffle(full_data)
+test_size = 0.2
+#prepare dictionaries for training and testing to be populated
+#dictioanries have two keys: 2 = benign tumors, 4 = malinant tumors
+train_set = {2:[], 4:[]}
+test_set = {2:[], 4:[]}
+#select first 80% as training data
+train_data = full_data[:-int(test_size*len(full_data))]
+#select last 20% as testing data
+test_data = full_data[-int(test_size*len(full_data)):]
