17b64feb53f2b1baceb8145c877dd57a3ed3b34b
Daniel Rappard <drappard@g.emporia.edu>
2019-02-06 14:33:03 -0600

    fixed import syntax, added svm package, listed output

--- svm1.py
+++ svm1.py
@@ -4,7 +4,7 @@
 #Use Scikit-Learn K Nearest Neighbors code fron knn1.py
 
 import numpy as np
-from sklearn import preprocessing, cross_validation, Neighbors
+from sklearn import preprocessing, cross_validation, neighbors, svm
 import pandas as pd
 
 df = pd.read_csv('breast-cancer-wisconsin.data')
@@ -16,7 +16,7 @@ y = np.array(df['class'])
 
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
 
-clf = neighbors.KNeighborsClassifier()
+clf = svm.SVC()
 
 clf.fit(X_train, y_train)
 confidence = clf.score(X_test, y_test)
@@ -25,4 +25,8 @@ print(confidence)
 example_measures = np.array([[4,2,1,1,1,2,3,2,1]])
 example_measures = example_measures.reshape(len(example_measures), -1)
 prediction = clf.predict(example_measures)
-print(confidence)
+print(prediction)
+
+#SVM Output:
+#Accuracy = 0.9714285714285714
+#Prediction = [2]
