633c4248b898c073777fa8d76247ae32e006d74a
Daniel Rappard <drappard@g.emporia.edu>
2019-05-09 15:01:00 -0500

    Create mnist_nn.py

---
+++ mnist_nn.py
@@ -0,0 +1,24 @@
+import tensorflow as tf
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+import matplotlib.pyplot as plt
+image_index = 7777
+print(y_train[image_index])
+plt.imshow(x_train[image_index], cmap='Greys')
+
+x_train.shape
+
+#Reshaping the array to a 4-dimensional so that it can work with the Keras API
+x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+
+#Making sure that the values are float so that we can get decimal points after division
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+
+#Normalizing the RGB codes by dividing it to the max RGB values
+x_train /= 255
+x_tes /= 255
+print('x_train shape:', x_train.shape)
+print('NUmber of images in x_train', x_train.shape[0])
+print('Number of images in x_test', x_test.shape[0])
