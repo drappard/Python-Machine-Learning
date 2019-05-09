dfc1c7ff7db964f458d8669fa893c457b23340eb
Daniel Rappard <drappard@g.emporia.edu>
2019-05-09 15:29:54 -0500

    Update mnist_nn.py
    following tutorial from https://towardsdatascience.com/image-classification-in-10-minutes-with-mnist-dataset-54c35b77a38d

--- mnist_nn.py
+++ mnist_nn.py
@@ -18,7 +18,27 @@ x_test = x_test.astype('float32')
 
 #Normalizing the RGB codes by dividing it to the max RGB values
 x_train /= 255
-x_tes /= 255
+x_test /= 255
 print('x_train shape:', x_train.shape)
 print('NUmber of images in x_train', x_train.shape[0])
 print('Number of images in x_test', x_test.shape[0])
+
+#Import the required Keras modules containing model and layers
+from keras.model import Sequential
+from keras.layers import Dense, Conv2D, Dropout, Flatten, MaxPooling2D
+#Creating a sequential model and adding the layers
+model = Sequential()
+model.add(Conv2d(28, kernel_size=(3,3), input_shape=input_shape))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Flatten()) #Flatten the 2D arrays for the fully connected layers
+model.add(Dense(128, activation=tf.nn.relu))
+model.add(Dropout(0.2))
+model.add(Dense(10,activation=tf.nn.softmax))
+
+
+model.compile(optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy'])
+model.fit(x=x_train, y=y_train, epochs=10)
+
+model.evauate(x_test, y_test)
