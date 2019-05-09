65e18e1d1435468967bd696b1cf742e7c73e2a23
Daniel Rappard <drappard@g.emporia.edu>
2019-05-09 15:01:47 -0500

    Update nn3.py
    attempted to fix import errors with different import of mnist

--- nn3.py
+++ nn3.py
@@ -1,9 +1,13 @@
 #Python Machine Learning Tutorial - Deep Learning with TensorFlow
 #Part 3 - How the Network Will Run
 
+import keras
 import tensorflow as tf
+from tensorflow import keras
+#mnist = tf.keras.datasets.mnist
+#(train_images, train_labels),(test_images, test_labels) = mnist.load_data()
 from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("/tmp/data/", one_hot = True)
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
 n_nodes_hl1 = 500
 n_nodes_hl2 = 500
@@ -43,19 +47,20 @@ def neural_network_model(data):
     return output
 
 def train_neural_network(x):
-    predictions = neural_network_model (x)
+    prediction = neural_network_model (x)
     cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y) )
     optimizer = tf.train.AdamOptimizer().minimize(cost)
 
     hm_epochs = 10
     with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer)
+        sess.run(tf.global_variables_initializer())
 
         for epochs in range(hm_epochs):
             epoch_loss = 0
-            for _ in range(int(mnist.train.num_examples/batch_size))
-            _, c = sess.run([optimizer,cost], feed_dict={x: epoch_x, y: epoch_y})
-            epoch_less += c
+            for _ in range(int(mnist.train.num_examples)):
+                epoch_x, epoch_y = mnist.train.next_batch(batch_size)
+                _, c = sess.run([optimizer,cost], feed_dict={x: epoch_x, y: epoch_y})
+                epoch_loss += c
 
         print('Epoch', epoch, 'completed out of', hm_epochs, 'loss', epoch_loss)
 
